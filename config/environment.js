e0d59acbd86f707ad6c7040d2ebf01fc81cea507
MoJie <support@onecoin.im>
2015-05-14 08:47:43 +0800

    修改 模块名称

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'website-onecoin-im',
+    modulePrefix: 'website',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
